55db6b90fb27b1ae039666244748dc0282d927e4
AliHESSEN <88345797+AliHESSEN@users.noreply.github.com>
2024-07-16 12:08:26 +0200

    Update homePage.jsx

--- startSide/homePage.jsx
+++ startSide/homePage.jsx
@@ -1,22 +1,20 @@
 import React from "react"; // Importerer React-biblioteket
 import Image from "./../images/gkmit.jpeg"; // Importerer et bilde fra den angitte banen
-//import Homepage from "./Homepage"; // (kommentert ut) Importerer en annen komponent
-import Navbar from "../Navbar/Navbar"; // Importerer Navbar-komponenten
 import "./Home.css"; // Importerer stilark for denne komponenten
 
 const Home = () => { // Definerer Home-komponenten
   return (
     <>
-      <section id="hero"> // Definerer en seksjon med id "hero"
-        <div className="heroContainer"> // En container-div for innholdet i hero-seksjonen
-          <div className="heroLogo"> // En div for logoen
-            <img src={Image} alt="testRestaurant is busy"/> // Viser bildet av restauranten
+      <section id="hero"> {/* Definerer en seksjon med id "hero" */}
+        <div className="heroContainer"> {/* En container-div for innholdet i hero-seksjonen */}
+          <div className="heroLogo"> {/* En div for logoen */}
+            <img src={Image} alt="testRestaurant is busy"/> {/* Viser bildet av restauranten */}
           </div>
-          <h1>Welcome To testRestaurant</h1> // Viser en velkomstmelding
-          <h2>Delight in every bite</h2> // Viser en underoverskrift
-          <div className="actions"> // En div for handlingselementer (knapper)
-            <a href="/login" className="main2">Login</a> // Lenke til innloggingssiden
-            <a href="/register" className="main1">Register</a> // Lenke til registreringssiden
+          <h1>Welcome To testRestaurant</h1> {/* Viser en velkomstmelding */}
+          <h2>Delight in every bite</h2> {/* Viser en underoverskrift */}
+          <div className="actions"> {/* En div for handlingselementer (knapper) */}
+            <a href="/login" className="main2">Login</a> {/* Lenke til innloggingssiden */}
+            <a href="/register" className="main1">Register</a> {/* Lenke til registreringssiden */}
           </div>
         </div>
       </section>
